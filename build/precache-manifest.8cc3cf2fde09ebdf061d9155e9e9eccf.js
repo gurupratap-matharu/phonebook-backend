094029f109f1aab8ede24801197ed897fca2e24e
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-07-02 16:03:18 -0300

    Production build ready

---
+++ build/precache-manifest.8cc3cf2fde09ebdf061d9155e9e9eccf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d0ebc316df729960f31875542d9155a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d748c1e5660f70429b5c",
+    "url": "/static/css/main.063c7c6e.chunk.css"
+  },
+  {
+    "revision": "db977e8610798d8a546c",
+    "url": "/static/js/2.a64c161a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a64c161a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d748c1e5660f70429b5c",
+    "url": "/static/js/main.00a83727.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
