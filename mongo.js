50773b8a8da75ad04ad47f0f2bdb4b4599b16f84
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-07-07 21:24:06 -0300

    added some cool new functionality to the app

--- mongo.js
+++ mongo.js
@@ -1,38 +1,48 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log('Please provide the password as an argument: node mongo.js <password>')
+    console.log('Please provide a password like this => node mongo.js <password>')
     process.exit(1)
 }
 
 const password = process.argv[2]
-const url = `mongodb+srv://testuser:${password}@cluster0.v5zhc.mongodb.net/note-app?retryWrites=true&w=majority`
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url = `mongodb+srv://testuser:${password}@cluster0.m5qvr.mongodb.net/phonebook-app?retryWrites=true&w=majority`
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
 
-const noteSchema = new mongoose.Schema({
-    content: String,
-    date: Date,
-    important: Boolean,
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: Number,
 })
 
-const Note = mongoose.model('Note', noteSchema)
+const Person = mongoose.model('Person', personSchema)
 
-const note = new Note({
-    content: 'HTML is Easy',
-    date: new Date(),
-    important: true,
-})
-
-// note.save().then(result => {
-//     console.log('note saved!')
-//     mongoose.connection.close()
-// })
-
-Note.find({})
-    .then(result => {
-        result.forEach(note => {
-            console.log(note)
+if (process.argv.length === 3) {
+    console.log('phonebook:')
+    Person.find({})
+        .then(result => {
+            result.forEach(person => {
+                console.log(`${person.name} ${person.number}`)
+            })
+            mongoose.connection.close()
         })
-        mongoose.connection.close()
+
+}
+else {
+    const person = Person({
+        name: name,
+        number: number,
     })
+    console.log('Veer sending...', person)
+
+    person.save()
+        .then(response => {
+            console.log('person saved!')
+            console.log(`Added ${name} number ${number} to phonebook`)
+            mongoose.connection.close()
+        })
+
+}
