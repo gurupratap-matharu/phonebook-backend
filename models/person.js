bec03ca291805f54d5de778751c394c00d8b5679
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-07-14 18:22:27 -0300

    hardened code for production

--- models/person.js
+++ models/person.js
@@ -1,33 +1,34 @@
-const mongoose = require("mongoose");
-const uniqueValidator = require("mongoose-unique-validator");
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
-mongoose.set("useFindAndModify", false);
+mongoose.set('useFindAndModify', false)
 
-const url = process.env.MONGODB_URI;
-console.log("connecting to ", url);
+const url = process.env.MONGODB_URI
+console.log('connecting to ', url)
 
 mongoose
   .connect(url, { useUnifiedTopology: true, useNewUrlParser: true })
   .then((result) => {
-    console.log("connected to MongoDB");
+    console.log('connected to MongoDB')
+    console.log(result)
   })
   .catch((error) => {
-    console.log("error connecting to MongodB", error.message);
-  });
+    console.log('error connecting to MongodB', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true, minlength: 3 },
   number: { type: String, required: true, unique: true, minlength: 8 },
-});
+})
 
-personSchema.plugin(uniqueValidator);
+personSchema.plugin(uniqueValidator)
 
-personSchema.set("toJSON", {
+personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model("Person", personSchema);
+module.exports = mongoose.model('Person', personSchema)
