2aca4bfae821545a91aa8808864634844f6281b9
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-07-15 14:45:43 -0300

    removed mongo connection from the person.js

--- models/person.js
+++ models/person.js
@@ -3,22 +3,19 @@ const uniqueValidator = require('mongoose-unique-validator')
 
 mongoose.set('useFindAndModify', false)
 
-const url = process.env.MONGODB_URI
-console.log('connecting to ', url)
-
-mongoose
-  .connect(url, { useUnifiedTopology: true, useNewUrlParser: true })
-  .then((result) => {
-    console.log('connected to MongoDB')
-    console.log(result)
-  })
-  .catch((error) => {
-    console.log('error connecting to MongodB', error.message)
-  })
-
 const personSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true, minlength: 3 },
-  number: { type: String, required: true, unique: true, minlength: 8 },
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 3
+  },
+  number: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 8
+  },
 })
 
 personSchema.plugin(uniqueValidator)
